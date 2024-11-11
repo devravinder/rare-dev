ed571a0d951adac784a1547190b32c0e9e796997
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2024-11-11 11:05:05 +0530

    feat: basic simple todo app with next15

---
+++ apps/learn-next-15/simple-todo/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import TodoItem from "./TodoItem";
+import { fetchTodos } from "@/actions/todoActions";
+export const fetchCache = 'force-no-store';
+
+const TodoList = async () => {
+    const todos = await fetchTodos();
+    return (<div className="flex flex-col gap-4 w-full">
+
+      {
+        todos.map((todo) => <TodoItem key={todo.id} {...todo} />)
+      }
+      {todos.length ==0 && <div className="flex flex-row justify-center items-center">No Todos</div>}
+  
+    </div>)
+  }
+
+export default TodoList;
